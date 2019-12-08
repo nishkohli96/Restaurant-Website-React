75924ec83a02c7143f82784cb9ba3fccf5777b2f
Nishant Kohli <nishkohli96@gmail.com>
2019-12-08 23:29:10 +0530

    components-cards and menus

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/Menu';
+import CardMenu from './components/CardMenu';
 import './App.css';
+import { DISHES } from './shared/dishes'
 
-function App() {
-  return (
-    <div >
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-      </div>
-    </Navbar>
-    <Menu />
-  </div>
-  );
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = { dishes : DISHES }
+  }
+
+  render()
+  {
+    return (
+      <div >
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <CardMenu dishes={this.state.dishes} />  
+        <Menu dishes={this.state.dishes} />
+    </div>
+    );
+  }
 }
 
 export default App;
