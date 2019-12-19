7c93ff7696a80ce6fcdb34f93d2e7ec334dd2b2f
Nishant Kohli <nishkohli96@gmail.com>
2019-12-20 04:36:39 +0530

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Main from './components/Main';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends React.Component {
 
   render()
   {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter> 
     );
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,32 +5,31 @@ import DishDetail from './Dishdetail';
 import { DISHES } from '../shared/dishes';
 import Header from './Header';
 import Footer from './Footer';
+import Home from './Home';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+        dishes: DISHES
     };
-    this.onDishSelect=this.onDishSelect.bind(this);
-  }
-
-  onDishSelect(dishId)
-  {
-    this.setState({ selectedDish: dishId });
-    console.log(this.state.selectedDish);
   }
 
   render() {
+    const HomePage = () =>{
+      return (<Home />);
+    }
+
     return (
       <div>
         <Header />
-        <CardMenu dishes={this.state.dishes} comments={DISHES[0].comments}/>
-         {/* onClick= {(dishId)=> this.onDishSelect(dishId) } /> */}
-        <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />  
-        {/* filter returns an array, so selecting the 1st element as the result */}
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <CardMenu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
